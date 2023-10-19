aa1c0f9eb715b40fe14d8eb6733927d6b78d4e37
shubhamatkal <shubhamatkal333@gmail.com>
2023-10-19 21:46:47 +0530

    import ball class in main

--- main.py
+++ main.py
@@ -29,6 +29,41 @@ level.load_panels(LEVEL)
 while game_is_on:
 
     screen.update()
+    time.sleep(0.01)
+    ball.move()
+
+    if not level.panel_list:
+        scoreboard.new_level()
+        ball.new_ball()
+        LEVEL += 1
+        level.load_panels(LEVEL)
+
+    # Detect Collision with panel
+    for panel in level.panel_list:
+        if ball.distance(panel) < 40:
+            ball.bounce_y()
+            level.panel_list.remove(panel)
+            panel.hideturtle()
+
+    # Detect collision with ceiling
+    if ball.ycor() > 280:
+        ball.bounce_y()
+
+    # Detect Collision with walls
+    if ball.xcor() > 380 or ball.xcor() < -380:
+        ball.bounce_x()
+
+    # Detect Collision with paddle
+    if ball.distance(pad) < 50 and ball.ycor() < -230:
+        ball.bounce_y()
+
+    # Detect paddle miss
+    if ball.ycor() < -280:
+        ball.new_ball()
+        lives.update_lives()
+        if lives.lives < 1:
+            scoreboard.you_lose()
+            game_is_on = False
 
 
 
